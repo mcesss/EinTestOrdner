b84c616993f1a3363d909da09c8074badb36e195
Markus Hoeft <mhoeft1@hs-mittweida.de>
2021-07-13 18:09:46 +0200

    alles

--- src/main/java/helloWorld/App.java
+++ src/main/java/helloWorld/App.java
@@ -1,7 +1,8 @@
 package helloWorld;
+import java.io.IOException;
+import com.github.lalyos.jfiglet.GigletFont;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("hi");
+    public static void main(String[] args) throws IOException {
     }
 }
